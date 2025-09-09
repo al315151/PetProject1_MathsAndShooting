b5f7a57d7f822113b9e139449932735f4a179c45
Albert Dols Ferrandis <albertdolsferrandis@gmail.com>
2025-09-09 23:43:49 +0200

    Added PlayerInteraction on InputManager
    Added Raycast to find the proper position on the game through the Camera.
    Also created new layer to be used only for this purpose (playerInteraction).
    
    TODO: get the character to move to the position defined by the raycast

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -5,11 +5,14 @@ using VContainer.Unity;
 
 public class InputManager : IInitializable, IDisposable, ITickable
 {
+    private readonly int interactionLayer = LayerMask.GetMask("PlayerInteraction");
+
     private readonly CameraProvider cameraProvider;
 
     public InputManager(CameraProvider cameraProvider)
     {
         this.cameraProvider = cameraProvider;
+
     }
 
     public void Initialize()
@@ -38,10 +41,38 @@ public class InputManager : IInitializable, IDisposable, ITickable
         // On mouse button being pressed, 
         if (Input.GetMouseButtonDown(0))
         {
-            var mousePosition = Input.mousePosition;
-            Debug.Log($"Input mouse position in pixels: {mousePosition}");
+            GetWorldPositionFromScreenPosition(Input.mousePosition);
+            
         }
     }
+
+    private void GetWorldPositionFromScreenPosition(Vector3 screenPosition)
+    {
+        var currentCamera = cameraProvider.MainCamera;
+        var originPosition = currentCamera.transform.position;
+
+        var endVectorPos = new Vector3(screenPosition.x, screenPosition.y, currentCamera.nearClipPlane);
+        var worldPosition = currentCamera.ScreenToWorldPoint(endVectorPos);
+
+        var direction = worldPosition - originPosition;
+
+        Debug.Log($"Input mouse position in pixels: {screenPosition}");
+        Debug.Log($"Input mouse position in world coordinates: {worldPosition}");
+
+        var raycastResults = new RaycastHit[5];
+        var numberOfHits = Physics.RaycastNonAlloc(new Ray(originPosition, direction), raycastResults, 20.0f, interactionLayer);
+        Debug.DrawRay(originPosition, direction, Color.red, 1.0f);
+
+        if (numberOfHits > 0)
+        {
+            for (var i = 0; i < numberOfHits; i++)
+            {
+                Debug.Log($"Position obtained by raycast: {raycastResults[i].point}");
+            }
+        }
+
+    }
+
 
 
 }
